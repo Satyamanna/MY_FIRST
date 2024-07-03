a1b73422d11b23d5dfdf30a9e9c1cc1f7ca37f64
ashokit <satyabratamanna5960@gmail.com>
2024-07-03 20:41:04 +0530

    2 change

--- wellcome.java
+++ wellcome.java
@@ -2,8 +2,14 @@ class My
 {
 	public static void main(String [] args)
 	{
+		//set
+		int i=100;
 		System.out.println("my name is khan");
+<<<<<<< Updated upstream
 		System.out.println("my name is khan");
 
+=======
+		
+>>>>>>> Stashed changes
 	}
 }
